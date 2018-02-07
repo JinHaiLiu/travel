061f82ff37eaef515032ebeb8e9616f4d1739185
JinHaiLiu <jh.liu@protonmail.com>
2018-02-07 11:50:05 +0800

    sql

---
+++ src/main/resources/sql/table.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.56 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `hotplace` (
+	`id` int (11),
+	`s_img` varchar (600),
+	`s_name` varchar (300),
+	`s_level` varchar (30),
+	`s_area` varchar (300),
+	`s_price` int (11),
+	`s_soldnum` int (11),
+	`sight_hot` int (10),
+	`s_replace` varchar (300),
+	`s_slogen` varchar (300)
+); 
